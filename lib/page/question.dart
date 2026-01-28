934f6e645be4eb73ff3ca21fdf11e90eb321d3f0
Nityanand Kumar <03nkumar01@gmail.com>
2026-01-28 19:40:32 +0530

    question.dart file is added

---
+++ lib/page/question.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class Question extends StatefulWidget {
+  const Question({super.key});
+
+  @override
+  State<Question> createState() => _QuestionState();
+}
+
+class _QuestionState extends State<Question> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+    );
+  }
+}
