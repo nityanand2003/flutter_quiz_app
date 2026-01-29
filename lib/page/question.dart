151b9a3270d798cc9db0ba86c10e1d8627fc3c86
Nityanand Kumar <03nkumar01@gmail.com>
2026-01-30 01:42:47 +0530

    Some changes in quesion.dart file

--- lib/page/question.dart
+++ lib/page/question.dart
@@ -11,7 +11,46 @@ class _QuestionState extends State<Question> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
+      backgroundColor: Color(0xFF004840),
+      body: Container(
+        child: Column(
+          children: [
+            Padding(
+              padding: EdgeInsets.only(top: 50.0, left: 20.0),
+              child: Row(
+                children: [
+                  Container(
+                    padding: EdgeInsets.all(6),
+                    decoration: BoxDecoration(
+                      color: Color(0xFFf35b32),
+                      borderRadius: BorderRadius.circular(60),
+                    ),
+                    child: Center(
+                      child: Icon(Icons.arrow_back_ios, color: Colors.white),
+                    ),
+                  ),
+                  SizedBox(width: 100.0),
+                  Text(
+                    "Sports",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 24.0,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(height: 20.0,),
+            Expanded(child: Container(
+              padding: EdgeInsets.only(left: 20.0),
+              width: MediaQuery.of(context).size.width,
+              decoration: BoxDecoration(color: Colors.white, borderRadius: BorderRadius.circular(60),),
+            )),
+            Container(decoration: BoxDecoration(color: Colors.white)),
+          ],
+        ),
+      ),
     );
   }
 }
